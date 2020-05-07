57e505a5e4c22b0bbd316597a964d87bffd948e9
Arthur <arthur@corenzan.com>
2020-05-06 22:12:53 -0300

    Reduce length of hashids (#6)

--- src/hooks/useRoomId.js
+++ src/hooks/useRoomId.js
@@ -5,7 +5,9 @@ import Hashids from "hashids";
 const hashids = new Hashids();
 
 function uniqueId() {
-  return hashids.encode(Date.now());
+  // This magic number is simply the value of Date.now() when I wrote this.
+  // It's meant to reduce the length of the hashid generated.
+  return hashids.encode(Date.now() - 1588812376880);
 }
 
 export function useRoomId(root = "/") {
