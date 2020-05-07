91fec07d628dae82751c579042964cf84811d593
Arthur <arthur@corenzan.com>
2020-05-06 22:12:38 -0300

    Fix input selection on focus (#4)

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import { useTimer } from "../../hooks/useTimer";
 import { useStoredState } from "../../hooks/useStoredState";
@@ -6,7 +6,15 @@ import { useRoomId } from "../../hooks/useRoomId";
 
 import style from "./index.module.css";
 
-function Input({ time, readOnly, setTime }) {
+function Input({ time, setTime }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (document.activeElement === ref.current) {
+      ref.current.setSelectionRange(0, ref.current.value.length);
+    }
+  });
+
   const value =
     (time > 3600
       ? String((time / 3600).toFixed()).padStart(2, "0") + ":"
@@ -15,26 +23,37 @@ function Input({ time, readOnly, setTime }) {
     ":" +
     String(time % 60).padStart(2, "0");
 
-  const handleChange = (e) => {
-    const time = e.target.value.split(":").map((n) => parseInt(n, 10));
-    setTime(time.pop() + time.pop() * 60 + (time.length ? time[0] * 3600 : 0));
-  };
+  // const handleChange = (e) => {
+  //   const time = e.target.value.split(":").map((n) => parseInt(n, 10));
+  //   setTime(time.pop() + time.pop() * 60 + (time.length ? time[0] * 3600 : 0));
+  // };
 
   const handleKeyDown = (e) => {
-    if (e.key === "ArrowUp") {
+    if (e.key === "Escape") {
+      e.target.blur();
+    } else if (e.key === "ArrowUp") {
       setTime(time + 60);
     } else if (e.key === "ArrowDown") {
       setTime(time > 60 ? time - 60 : 0);
+    } else {
+      return;
     }
+
+    e.preventDefault();
+  };
+
+  const handleFocus = (e) => {
+    e.target.setSelectionRange(0, e.target.value.length);
   };
 
   return (
     <input
+      ref={ref}
       type="text"
       value={value}
-      onChange={handleChange}
+      onFocus={handleFocus}
       onKeyDown={handleKeyDown}
-      readOnly={readOnly}
+      readOnly={true}
       style={{ width: `${value.length}ch` }}
     />
   );
